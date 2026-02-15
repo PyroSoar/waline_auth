51f2c823e4c1039faf4b2f931abcdb44610cdcc9
lzctuhao <lzctuhao@outlook.com>
2026-02-15 17:34:21 +0800

    Fixed QQ login

--- src/qq.js
+++ src/qq.js
@@ -1,27 +1,27 @@
 const Base = require('./base');
 const qs = require('querystring');
 const request = require('request-promise-native');
+const jwt = require('jsonwebtoken');
 
 const OAUTH_URL = 'https://graph.qq.com/oauth2.0/authorize';
 const ACCESS_TOKEN_URL = 'https://graph.qq.com/oauth2.0/token';
 const TOKEN_INFO_URL = 'https://graph.qq.com/oauth2.0/me';
 const USER_INFO_URL = 'https://graph.qq.com/user/get_user_info';
 
-const {QQ_ID, QQ_SECRET} = process.env;
+const { QQ_ID, QQ_SECRET } = process.env;
+
 module.exports = class extends Base {
   static check() {
     return QQ_ID && QQ_SECRET;
   }
-  
+
   static info() {
-    return {
-      origin: new URL(OAUTH_URL).hostname
-    };
+    return { origin: new URL(OAUTH_URL).hostname };
   }
-  
+
   redirect() {
-    const {redirect, state} = this.ctx.params;
-    const redirectUrl = this.getCompleteUrl('/qq') + '?' + qs.stringify({redirect, state});
+    const { redirect, state } = this.ctx.params;
+    const redirectUrl = this.getCompleteUrl('/qq') + '?' + qs.stringify({ redirect, state });
 
     const url = OAUTH_URL + '?' + qs.stringify({
       client_id: QQ_ID,
@@ -31,77 +31,45 @@ module.exports = class extends Base {
     return this.ctx.redirect(url);
   }
 
-  async getAccessToken(code) {
-    const {redirect, state} = this.ctx.params;
-    const redirectUrl = this.getCompleteUrl('/qq') + '?' + qs.stringify({redirect, state});
-
-    const params = {
-      client_id: QQ_ID,
-      client_secret: QQ_SECRET,
-      grant_type: 'authorization_code',
-      redirect_uri: redirectUrl,
-      code,
-      fmt: 'json'
-    };
-
-    const response = await request.post({
-      url: ACCESS_TOKEN_URL,
-      form: params,
-      json: true
-    });
-
-    // Check for QQ API error
-    if (response.error) {
-      const err = new Error(`[QQ API Error] ${response.error_description || response.error}`);
-      err.code = response.error;
-      throw err;
+  async indexAction() {
+    const { code, redirect, state } = this.ctx.params;
+    if (!code) {
+      return this.redirect();
     }
 
-    return response;
+    // Step 1: Exchange code for access token
+    const tokenResponse = await this.getAccessToken(code);
+
+    // Step 2: Fetch user info
+    const user = await this.getUserInfoByToken(tokenResponse);
+
+    // Step 3: Check or create DB record
+    const userByQQ = await this.modelInstance.select({ qq: user.id });
+    let objectId;
+    if (!think.isEmpty(userByQQ)) {
+      objectId = userByQQ[0].objectId;
+    } else {
+      const created = await this.modelInstance.add({
+        display_name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+        qq: user.id,
+        password: this.hashPassword(Math.random()),
+        type: 'guest',
+      });
+      objectId = created.objectId;
+    }
+
+    // Step 4: Issue JWT
+    const token = jwt.sign(objectId, this.config('jwtKey'));
+
+    // Step 5: Redirect with token
+    if (redirect) {
+      this.redirect(redirect + (redirect.includes('?') ? '&' : '?') + 'token=' + token);
+    } else {
+      this.success({ token });
+    }
   }
 
-  async getUserInfoByToken({access_token}) {
-    const tokenInfo = await request.get(TOKEN_INFO_URL + '?' + qs.stringify({
-      access_token,
-      unionid: 1,
-      fmt: 'json'
-    }), {json: true});
-
-    // Check for QQ API error response (QQ returns errcode on error)
-    if (tokenInfo.errcode) {
-      const err = new Error(`[QQ Token Error] ${tokenInfo.errmsg || `errcode: ${tokenInfo.errcode}`}`);
-      err.code = tokenInfo.errcode;
-      err.statusCode = 401;
-      throw err;
-    }
-
-    if (!tokenInfo.unionid || !tokenInfo.openid) {
-      const err = new Error('[QQ Token Error] Missing unionid or openid in response');
-      err.statusCode = 400;
-      throw err;
-    }
-
-    const userInfo = await request.get(USER_INFO_URL + '?' + qs.stringify({
-      access_token, 
-      openid: tokenInfo.openid,
-      oauth_consumer_key: tokenInfo.client_id,
-      format: 'json',
-    }), {json: true});
-
-    // Check for QQ user info API error (QQ returns ret != 0 on error)
-    if (userInfo.ret !== 0) {
-      const err = new Error(`[QQ UserInfo Error] ${userInfo.msg || `ret: ${userInfo.ret}`}`);
-      err.code = userInfo.ret;
-      err.statusCode = 401;
-      throw err;
-    }
-
-    return this.formatUserResponse({
-      id: tokenInfo.unionid,
-      name: userInfo.nickname || 'QQ User',
-      email: userInfo.email || undefined,
-      url: undefined,
-      avatar: userInfo.figureurl_qq_2 || userInfo.figureurl_qq_1 || userInfo.figureurl_qq || userInfo.figureurl_2 || userInfo.figureurl_1 || userInfo.figureurl || '',
-    }, 'qq');
-  }
-}
+  // keep getAccessToken and getUserInfoByToken as you already have
+};
