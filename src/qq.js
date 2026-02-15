f39a7d4151ecf5f347c8fa97e8e4eca55b2f0d59
lzctuhao <lzctuhao@outlook.com>
2026-02-15 17:41:37 +0800

    Fixed QQ login

--- src/qq.js
+++ src/qq.js
@@ -40,7 +40,7 @@ module.exports = class extends Base {
     // Step 1: Exchange code for access token
     const tokenResponse = await this.getAccessToken(code);
 
-    // Step 2: Fetch user info
+    // Step 2: Fetch QQ user info
     const user = await this.getUserInfoByToken(tokenResponse);
 
     // Step 3: Check or create DB record
@@ -71,5 +71,74 @@ module.exports = class extends Base {
     }
   }
 
-  // keep getAccessToken and getUserInfoByToken as you already have
+  async getAccessToken(code) {
+    const { redirect, state } = this.ctx.params;
+    const redirectUrl = this.getCompleteUrl('/qq') + '?' + qs.stringify({ redirect, state });
+
+    const params = {
+      client_id: QQ_ID,
+      client_secret: QQ_SECRET,
+      grant_type: 'authorization_code',
+      redirect_uri: redirectUrl,
+      code,
+      fmt: 'json'
+    };
+
+    const response = await request.post({
+      url: ACCESS_TOKEN_URL,
+      form: params,
+      json: true
+    });
+
+    if (response.error) {
+      const err = new Error(`[QQ API Error] ${response.error_description || response.error}`);
+      err.code = response.error;
+      throw err;
+    }
+
+    return response;
+  }
+
+  async getUserInfoByToken({ access_token }) {
+    const tokenInfo = await request.get(TOKEN_INFO_URL + '?' + qs.stringify({
+      access_token,
+      unionid: 1,
+      fmt: 'json'
+    }), { json: true });
+
+    if (tokenInfo.errcode) {
+      const err = new Error(`[QQ Token Error] ${tokenInfo.errmsg || `errcode: ${tokenInfo.errcode}`}`);
+      err.code = tokenInfo.errcode;
+      err.statusCode = 401;
+      throw err;
+    }
+
+    if (!tokenInfo.unionid || !tokenInfo.openid) {
+      const err = new Error('[QQ Token Error] Missing unionid or openid in response');
+      err.statusCode = 400;
+      throw err;
+    }
+
+    const userInfo = await request.get(USER_INFO_URL + '?' + qs.stringify({
+      access_token,
+      openid: tokenInfo.openid,
+      oauth_consumer_key: tokenInfo.client_id,
+      format: 'json',
+    }), { json: true });
+
+    if (userInfo.ret !== 0) {
+      const err = new Error(`[QQ UserInfo Error] ${userInfo.msg || `ret: ${userInfo.ret}`}`);
+      err.code = userInfo.ret;
+      err.statusCode = 401;
+      throw err;
+    }
+
+    return {
+      id: tokenInfo.unionid,
+      name: userInfo.nickname || 'QQ User',
+      email: userInfo.email || undefined,
+      url: undefined,
+      avatar: userInfo.figureurl_qq_2 || userInfo.figureurl_qq_1 || userInfo.figureurl_qq || userInfo.figureurl_2 || userInfo.figureurl_1 || userInfo.figureurl || '',
+    };
+  }
 };
