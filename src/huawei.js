c0dcc0f262a72b0de32f23d7b4b72cf04927b96e
lzctuhao <lzctuhao@outlook.com>
2026-02-20 11:26:16 +0800

    Testing Huawei Auth.

--- src/huawei.js
+++ src/huawei.js
@@ -4,14 +4,14 @@ const request = require('request-promise-native');
 
 const OAUTH_URL = 'https://oauth-login.cloud.huawei.com/oauth2/v3/authorize';
 const ACCESS_TOKEN_URL = 'https://oauth-login.cloud.huawei.com/oauth2/v3/token';
-const USER_INFO_URL = 'https://account.cloud.huawei.com/user/getUserInfo';
+const USER_INFO_URL = 'https://oauth-login.cloud.huawei.com/oauth2/v3/userinfo';
 
-const { HUAWEI_ID, HUAWEI_SECRET } = process.env;
+const { HUAWEI_CLIENT_ID, HUAWEI_CLIENT_SECRET } = process.env;
 
 module.exports = class extends Base {
 
   static check() {
-    return HUAWEI_ID && HUAWEI_SECRET;
+    return HUAWEI_CLIENT_ID && HUAWEI_CLIENT_SECRET;
   }
 
   static info() {
@@ -20,37 +20,28 @@ module.exports = class extends Base {
     };
   }
 
-  /**
-   * Step 1: exchange code -> access_token
-   */
   async getAccessToken(code) {
 
-    const redirect = this.ctx.params.redirect;
-    const state = this.ctx.params.state;
+    const { redirect, state } = this.ctx.params;
 
     const redirectUrl =
       this.getCompleteUrl('/huawei') +
       '?' +
       qs.stringify({ redirect, state });
 
-    const params = {
-      grant_type: 'authorization_code',
-      client_id: HUAWEI_ID,
-      client_secret: HUAWEI_SECRET,
-      code,
-      redirect_uri: redirectUrl
-    };
-
     return request.post({
       url: ACCESS_TOKEN_URL,
-      form: params,
+      form: {
+        grant_type: 'authorization_code',
+        client_id: HUAWEI_CLIENT_ID,
+        client_secret: HUAWEI_CLIENT_SECRET,
+        code,
+        redirect_uri: redirectUrl
+      },
       json: true
     });
   }
 
-  /**
-   * Step 2: access_token -> user info
-   */
   async getUserInfoByToken({ access_token }) {
 
     const userInfo = await request.get({
@@ -62,27 +53,22 @@ module.exports = class extends Base {
     });
 
     /**
-     * Huawei typical response:
-     * {
-     *   userId: "xxx",
-     *   displayName: "xxx",
-     *   email: "xxx",
-     *   photoURL: "xxx"
-     * }
+     * Huawei returns OpenID Connect standard fields:
+     * sub
+     * name
+     * picture
+     * email
      */
 
     return this.formatUserResponse({
-      id: userInfo.userId,
-      name: userInfo.displayName || userInfo.userId,
-      email: userInfo.email || undefined,
-      url: undefined,
-      avatar: userInfo.photoURL
+      id: userInfo.sub,
+      name: userInfo.name || userInfo.sub,
+      email: userInfo.email,
+      avatar: userInfo.picture,
+      url: undefined
     }, 'huawei');
   }
 
-  /**
-   * Step 0: redirect to Huawei login
-   */
   async redirect() {
 
     const { redirect, state } = this.ctx.params;
@@ -96,7 +82,7 @@ module.exports = class extends Base {
       OAUTH_URL +
       '?' +
       qs.stringify({
-        client_id: HUAWEI_ID,
+        client_id: HUAWEI_CLIENT_ID,
         redirect_uri: redirectUrl,
         response_type: 'code',
         scope: 'openid profile email'
